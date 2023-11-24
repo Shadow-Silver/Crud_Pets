c6d9afef3b3164fd3de2cdf9e5f8c1fc0513f336
Mavro <sky.redline0101@gmail.com>
2023-11-24 04:03:35 -0500

    Se creo base de datos + el access data objects dao

--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -1,6 +1,28 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-
-  alert(1238839)
+cargarUsuarios();
   $('#usuarios').DataTable();
 });
+
+async function cargarUsuarios() {
+
+  const request = await fetch("usuario/234", {
+    method: "GET",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+});
+  const usuarios = await request.json();
+
+  let ListadoHtml='';
+
+  for (let usuario of usuarios) {
+
+    console.log(usuarios);
+    let usuarioHtml = '<tr><td>'+usuario.id+'</td><td>' + usuario.nombre + '' + usuario.nombre + '</td><td>' + usuario.email + '</td> <td>' + usuario.telefono + '</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"> <i class="fas fa-trash"></i></a> </td> </tr>'
+
+  ListadoHtml+= usuarioHtml;
+  }
+document.querySelector('#usuarios tbody').outerHTML= ListadoHtmlS;
+}
