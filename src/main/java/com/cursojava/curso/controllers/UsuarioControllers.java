6ea967f7826d254acad39899b11ec560242654c1
Mavro <sky.redline0101@gmail.com>
2023-11-24 00:30:16 -0500

    Se va relacionando el js, html, y las clases

--- src/main/java/com/cursojava/curso/controllers/UsuarioControllers.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioControllers.java
@@ -1,5 +1,7 @@
 package com.cursojava.curso.controllers;
 
+import com.cursojava.curso.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UsuarioControllers {
 
-    @RequestMapping(value = "prueba")
-    public String prueba(){
-        return "prueba";
+    // METODOS X CADA UNA DE LAS FUNCIONES
+    @RequestMapping(value = "usuario/{id}") //enruta lo que queremos ver en el nave  //SI QUEREMOS BUSCAR UN USUARIO EN ESPECIFICO ES USUARIO/#ID
+    public Usuario getUsuario(@PathVariable long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setTelefono("234234249");
+        return usuario;
+    }
+    @RequestMapping(value = "usuario999")
+    public Usuario editar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setTelefono("234234249");
+        return usuario;
+    }
+    @RequestMapping(value = "usuario09") //enruta lo que queremos ver en el nave
+    public Usuario eliminar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setTelefono("234234249");
+        return usuario;
+    }
+    @RequestMapping(value = "usuario84") //enruta lo que queremos ver en el nave
+    public Usuario buscar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setTelefono("234234249");
+        return usuario;
     }
 }
---
+++ src/main/resources/static/js/usuarios.js
@@ -0,0 +1,6 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+
+  alert(1238839)
+  $('#usuarios').DataTable();
+});
